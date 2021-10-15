62a0b80071d7d677fd80564c0c7be29adad93db7
Shaqri <peter200970@hotmail.com>
2021-10-15 17:40:43 -0500

    Add style to post form and handle likes of non signed users

--- app/controllers/posts/likes_controller.rb
+++ app/controllers/posts/likes_controller.rb
@@ -2,10 +2,14 @@ class Posts::LikesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_post
   def create
+    if current_user
     @post.likes.where(user_id: current_user.id).first_or_create
     respond_to do |format|
       format.html {redirect_to @post}
       format.js 
+    end
+    else 
+      redirect_to :new_user_session
     end
   end
 
@@ -18,6 +22,12 @@ class Posts::LikesController < ApplicationController
   end
 
   private
+
+  def authenticate_user!
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
 
   def set_post
     @post = Post.find(params[:post_id])
